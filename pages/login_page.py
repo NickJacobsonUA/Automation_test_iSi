09dd021b99bee68eb92e1594ce5a9dfa6295ef23
NickJacobsonUA <mykyta.yakovenko.eu@gmail.com>
2024-01-23 09:59:01 +0200

    Updated files: login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from locators.login_page_locators import LoginPageLocators
 from pages.base_page import BasePage
+from creds_and_links import Credentials
 
 
 class LoginPage(BasePage):
@@ -7,8 +8,10 @@ class LoginPage(BasePage):
     locators = LoginPageLocators
 
     def fill_login_form(self):
-        self.element_is_present(self.locators.USERNAME).send_keys("nick_test9")
-        self.element_is_visible(self.locators.PASSWORD).send_keys("000000")
+        username = Credentials.username
+        password = Credentials.passwords
+        self.element_is_present(self.locators.USERNAME).send_keys(username)
+        self.element_is_visible(self.locators.PASSWORD).send_keys(password)
         self.element_is_visible(self.locators.LOGIN_BUTTON).click()
         self.element_is_visible(self.locators.DISPATCHING)
 
