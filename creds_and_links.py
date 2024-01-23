5a04818e473a972d7c262a3d3ade5a08fc3739a5
NickJacobsonUA <mykyta.yakovenko.eu@gmail.com>
2024-01-23 09:56:53 +0200

    Created file creds_and_links.py

---
+++ creds_and_links.py
@@ -0,0 +1,13 @@
+class Links:
+    login = 'https://test9.isi-technology.com/#!/login'
+    dispatching = 'https://test9.isi-technology.com/#!/main'
+    vehicles = 'https://test9.isi-technology.com/#!/vehicles'
+    users = 'https://test9.isi-technology.com/#!/users'
+    driver_groups = 'https://test9.isi-technology.com/#!/groups'
+    passengers = 'https://test9.isi-technology.com/#!/clients'
+    payers = 'https://test9.isi-technology.com/#!/accounts'
+
+
+class Credentials:
+    username = 'nick_test9'
+    passwords = '000000'
