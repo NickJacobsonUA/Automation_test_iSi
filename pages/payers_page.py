9576fad8f1c05d5a57117df5aa34b24c1f9a7cad
NickJacobsonUA <mykyta.yakovenko.eu@gmail.com>
2024-03-14 17:28:32 +0200

    Updated main files: driver_group_page.py, driver_group_page_locators.py, driver_group_test.py

--- pages/payers_page.py
+++ pages/payers_page.py
@@ -1,52 +1,108 @@
 import time
+from selenium.webdriver.common.keys import Keys
 
 from generator.generator import generated_data
-from locators.payer_page_locators import AddPayerPageLocators
+from locators.payer_page_locators import AddPayerPageLocators, PayersListPageLocators, PayersPricingPageLocators
 from pages.base_page import BasePage
-from selenium.webdriver.common.keys import Keys
+import allure
 
 
 class AddPayerPage(BasePage):
 
     locators = AddPayerPageLocators
 
+    @allure.step("Fill Add Payer Form")
     def fill_add_payer_form(self):
         data = next(generated_data())  # generating data
         id_number = data.id_number
-        self.element_is_visible(self.locators.ADD_PAYER_BUTTON).click()
-
-        self.element_is_visible(self.locators.PAYER_ID).send_keys(id_number)
-        self.element_is_visible(self.locators.PAYER_NAME).send_keys(data.company)
-
-        # pricing_tab = self.element_is_visible(self.locators.PRICING_TAB)
-        # self.driver.switch_to.default_content()
-        # self.element_is_visible(self.locators.PRICING_TAB).click()
-        # self.element_is_visible(self.locators.AMBULATORY_DEFAULT).click()
-        # self.element_is_visible(self.locators.ADD_PARAMETER).click()
-        # self.element_is_visible(self.locators.PER_MILE_RATE_AM).send_keys('5')
-        # self.element_is_visible(self.locators.WHEELCHAIR_DEFAULT).click()
-        # self.element_is_visible(self.locators.PER_MILE_RATE_WH).send_keys('10')
-        # self.element_is_visible(self.locators.STRETCHER_DEFAULT).click()
-        # self.element_is_visible(self.locators.PER_MILE_RATE_ST).send_keys('20')
-        # self.element_is_visible(self.locators.SAVE_BUTTON_PRICING).click()
-        # Back to the Payer info
-        #self.element_is_visible(self.locators.INFO_TAB).click()
-
-        self.element_is_visible(self.locators.PAYMENT_METHOD_TOGGLE).click()
-        self.element_is_visible(self.locators.PRIVATE_PAY_TOGGLE).click()
-        self.element_is_visible(self.locators.CREDIT_CARD_TOGGLE).click()
-        self.element_is_visible(self.locators.CASH_TOGGLE).click()
-        #self.element_is_visible(self.locators.CHECK_TOGGLE).click()
-        #self.element_is_visible(self.locators.TAXI_TOGGLE).click()
-        self.element_is_visible(self.locators.AMBULATORY_TOGGLE).click()
-        self.element_is_visible(self.locators.WHEELCHAIR_TOGGLE).click()
-        self.element_is_visible(self.locators.STRETCHER_TOGGLE).click()
-        self.element_is_visible(self.locators.SAVE_BUTTON).click()
-        return id_number
-
-    def check_add_payer(self):
+        payer_name = data.company
+        with allure.step(f"Fill Add Payer Form"):
+            self.element_is_visible(self.locators.ADD_PAYER_BUTTON).click()
+            self.element_is_visible(self.locators.PAYER_ID).send_keys(id_number)
+            self.element_is_visible(self.locators.PAYER_NAME).send_keys(payer_name)
+            self.element_is_visible(self.locators.PAYMENT_METHOD_TOGGLE).click()
+            self.element_is_visible(self.locators.PRIVATE_PAY_TOGGLE).click()
+            self.element_is_visible(self.locators.CREDIT_CARD_TOGGLE).click()
+            self.element_is_visible(self.locators.CASH_TOGGLE).click()
+            self.element_is_visible(self.locators.AMBULATORY_TOGGLE).click()
+            self.element_is_visible(self.locators.WHEELCHAIR_TOGGLE).click()
+            self.element_is_visible(self.locators.STRETCHER_TOGGLE).click()
+        with allure.step(f"click on save button"):
+            self.element_is_visible(self.locators.SAVE_BUTTON).click()
         result = self.element_is_present(self.locators.RESULT).text
-        return result
+
+        return id_number, result
+
+    @allure.step("Unfreeze Payer")
+    def unfreeze_payer(self, payer):
+        with allure.step(f"search for payer"):
+            self.element_is_visible(self.locators.SEARCH).send_keys(payer)
+            self.element_is_visible(self.locators.PAYER_SELECT).click()
+        self.element_is_visible(self.locators.PRICING_TAB).click()
+        with allure.step(f"click on unfreeze button"):
+            self.element_is_visible(self.locators.UNFREEZE_BUTTON).click()
+        status = self.element_is_visible(self.locators.UNFROZEN_STATUS).text
+
+        return payer, status
+
+
+class PayersListPage(BasePage):
+
+    locators = PayersListPageLocators
+
+    @allure.step("Deactivate new Payer")
+    def deactivate_new_payer(self):
+        self.element_is_visible(self.locators.DEACTIVATE_BUTTON).click()
+        time.sleep(1) # Here I use regular wait() because of the front end defence against fast clicking
+
+        with allure.step(f"click on confirm button"):
+            self.element_is_visible(self.locators.CONFIRM).click()
+        status = self.element_is_visible(self.locators.PAYER_STATUS_INACTIVE).text
+        payer = self.element_is_visible(self.locators.PAYER_NAME_OUTPUT).text
+
+        return payer, status
+
+    @allure.step("Activate new Payer")
+    def activate_new_payer(self):
+        with allure.step(f"click on activate button"):
+            self.element_is_visible(self.locators.ACTIVATE_BUTTON).click()
+        status = self.element_is_visible(self.locators.PAYER_STATUS_ACTIVE).text
+        payer = self.element_is_visible(self.locators.PAYER_NAME_OUTPUT).text
+
+        return payer, status
+
+
+class PayerPricingPage(BasePage):
+
+    locators = PayersPricingPageLocators
+
+    @allure.step("Set Payer Pricing")
+    def set_payer_pricing(self, payer):
+
+        self.element_is_visible(self.locators.SEARCH).send_keys(payer)
+        self.element_is_visible(self.locators.PAYER_STATUS_ACTIVE).click()
+        self.element_is_visible(self.locators.PRICING_TAB).click()
+        with allure.step(f"Set Payer Pricing"):
+            self.element_is_visible(self.locators.AMBULATORY_DEFAULT).click()
+            self.element_is_visible(self.locators.ADD_PARAMETER).click()
+            self.element_is_visible(self.locators.PER_MILE_RATE_AM).send_keys(Keys.BACKSPACE)
+            self.element_is_visible(self.locators.PER_MILE_RATE_AM).send_keys('5')
+        with allure.step(f"click on save button"):
+            self.element_is_visible(self.locators.SAVE_BUTTON_PRICING).click()
+        time.sleep(1) # Here I use regular wait() because click happens too fast
+        with allure.step(f"click on confirm button"):
+            self.element_is_visible(self.locators.CONFIRM).click()
+        with allure.step(f"click on recalculate button"):
+            self.element_is_visible(self.locators.RECALCULATE).click()
+        result = self.element_is_visible(self.locators.RESULT).text
+        payer = self.element_is_visible(self.locators.PAYER_NAME_OUTPUT).text
+
+        return payer, result
+
+
+
+
+
 
 
 
--- tests/payers_test.py
+++ tests/payers_test.py
@@ -1,18 +1,61 @@
-from pages.payers_page import AddPayerPage
+from pages.payers_page import AddPayerPage, PayersListPage, PayerPricingPage
 from conftest import driver
 from pages.login_page import LoginPage
+from links import Links
+import allure
+import pytest
 
 
+@allure.feature("Add Payer")
 class TestAddPayer:
 
+    payer = []
+
+    @pytest.mark.skip(reason="not implemented")
+    @pytest.mark.regression
+    @allure.title("Check Add Payer")
     def test_add_payer_with_segments(self, driver):
-        login = LoginPage(driver, "https://test9.isi-technology.com/#!/login")
+        login = LoginPage(driver, Links.login)
         login.open()
         login.fill_login_form()
-        add_payer = AddPayerPage(driver, "https://test9.isi-technology.com/#!/accounts")
+        add_payer = AddPayerPage(driver, Links.payers)
         add_payer.open()
-        payer_id = add_payer.fill_add_payer_form()
-        result = add_payer.check_add_payer()
-        print(payer_id)
-        print(result)
+        payer_id, result = add_payer.fill_add_payer_form()
+        unfrozen_payer_id, status = add_payer.unfreeze_payer(payer_id)
+        self.payer.append(unfrozen_payer_id)
+        print(f'Payer {result}')
+        print(f'Payer {unfrozen_payer_id} has status "{status}". Expected result - "Unfreeze manual"')
         assert result == f'#{payer_id} has been created.', 'The payer was not created'
+        assert status == 'Unfreeze manual', 'The payer was not unfrozen'
+
+    @pytest.mark.skip(reason="not implemented")
+    @pytest.mark.regression
+    @allure.title("Check Deactivate Payer with Segments")
+    def test_deactivate_payer_with_segments(self, driver):
+
+        deactivated_payer = PayersListPage(driver, Links.payers)
+        deactivated_payer.open()
+        inactive_payer, status = deactivated_payer.deactivate_new_payer()
+        print(f'Payer {inactive_payer} is now {status}. Expected result - Inactive .')
+        assert status == 'Inactive', 'The Payer was not deactivated'
+
+    @pytest.mark.skip(reason="not implemented")
+    @allure.title("Check Activate Payer with Segments")
+    def test_activate_payer_with_segments(self, driver):
+
+        activated_payer = PayersListPage(driver, Links.payers)
+        activated_payer.open()
+        active_payer, status = activated_payer.activate_new_payer()
+        print(f'Payer {active_payer} is now {status}. Expected result - Active .')
+        assert status == 'Active', 'The Payer was not activated'
+
+    @pytest.mark.skip(reason="not implemented")
+    @allure.title("Check Set Pricing for Payer with Segments")
+    def test_set_pricing_for_payer_with_segments(self, driver):
+
+        payer = self.payer
+        pricing = PayerPricingPage(driver, Links.payers)
+        pricing.open()
+        payer, result = pricing.set_payer_pricing(payer)
+        print(f'{payer}s pricing was set.')
+        assert result in 'Orders, Payer invoices, Driver Group invoices were recalculated.'
