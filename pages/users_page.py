a1a51f93c61e43cdccfd1c43f0ca339d8b468fc6
NickJacobsonUA <mykyta.yakovenko.eu@gmail.com>
2024-03-14 17:29:00 +0200

    Updated main files: users_page.py, users_test.py

--- pages/users_page.py
+++ pages/users_page.py
@@ -1,62 +1,60 @@
 from generator.generator import generated_data
 from locators.users_page_locators import AddDriverPageLocators
 from pages.base_page import BasePage
+import allure
 
 
 class AddUserPage(BasePage):
 
     locators = AddDriverPageLocators
 
+    @allure.step("Fill Add Simple Driver Form")
     def fill_add_simple_driver_form(self):
         # generating data
         person = next(generated_data())
         username = person.username
-        self.element_is_visible(self.locators.ADD_USER_BUTTON).click()
-        self.element_is_visible(self.locators.FIRST_NAME).send_keys(person.firstname)
-        self.element_is_visible(self.locators.LAST_NAME).send_keys(person.lastname)
-        self.element_is_visible(self.locators.USER_TYPE_SELECT).click()
-        self.element_is_present(self.locators.USER_TYPE_INPUT).click()
-        self.element_is_present(self.locators.DRIVER_TYPE_SELECT).click()
-        self.element_is_present(self.locators.DRIVER_TYPE_INPUT).click()
-        self.element_is_visible(self.locators.ADDRESS1).send_keys(person.address_buffalo)
-        self.element_is_present(self.locators.ADDRESS1_SELECT).click()
-        self.element_is_visible(self.locators.BIRTHDAY_INPUT).click()
-        self.element_is_visible(self.locators.BIRTHDAY_YEAR_SEARCH).click()
-        self.element_is_visible(self.locators.BIRTHDAY_YEAR_SEARCH).click()
-        self.element_is_visible(self.locators.BIRTHDAY_YEAR_SELECT).click()
-        self.element_is_visible(self.locators.BIRTHDAY_MONTH_SELECT).click()
-        self.element_is_visible(self.locators.BIRTHDAY_DAY_SELECT).click()
-
-        self.element_is_visible(self.locators.PHONE1).send_keys(person.mobile)
-
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_INPUT).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_SEARCH).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_SEARCH).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_SEARCH_RIGHT).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_YEAR_SELECT).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_MONTH_SELECT).click()
-        self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_DAY_SELECT).click()
-
-        self.element_is_visible(self.locators.LICENCE_NUMBER).send_keys(person.mobile)
-
-        self.element_is_visible(self.locators.LICENCE_TYPE_SELECT).click()
-        self.element_is_visible(self.locators.LICENCE_TYPE_OPTION).click()
-
-        self.element_is_visible(self.locators.HIRE_DATE_INPUT).click()
-        self.element_is_visible(self.locators.HIRE_DATE_INPUT_DAY_SELECT).click()
-
-        self.element_is_visible(self.locators.ROUTE_WITH_SEGMENTS).click()
-        self.element_is_visible(self.locators.CAN_SEE_AVAILABLE_TRIPS_SELECT).click()
-        self.element_is_visible(self.locators.CAN_SEE_AVAILABLE_TRIPS_OPTION).click()
-
-        self.element_is_visible(self.locators.EMAIL).send_keys(person.email)
-        self.element_is_visible(self.locators.USER_NAME).send_keys(username)
-
-        self.element_is_visible(self.locators.PASSWORD).send_keys(person.password)
-        self.element_is_visible(self.locators.CONFIRM_PASSWORD).send_keys(person.password)
-        self.element_is_visible(self.locators.SAVE_BUTTON).click()
+        with allure.step(f"click on Add User button"):
+            self.element_is_visible(self.locators.ADD_USER_BUTTON).click()
+        with allure.step(f"Filling Simple Driver with segments Form"):
+            self.element_is_visible(self.locators.FIRST_NAME).send_keys(person.firstname)
+            self.element_is_visible(self.locators.LAST_NAME).send_keys(person.lastname)
+            self.element_is_visible(self.locators.USER_TYPE_SELECT).click()
+            self.element_is_present(self.locators.USER_TYPE_INPUT).click()
+            self.element_is_present(self.locators.DRIVER_TYPE_SELECT).click()
+            self.element_is_present(self.locators.DRIVER_TYPE_INPUT).click()
+            self.element_is_visible(self.locators.ADDRESS1).send_keys(person.address_buffalo)
+            self.element_is_present(self.locators.ADDRESS1_SELECT).click()
+            self.element_is_visible(self.locators.BIRTHDAY_INPUT).click()
+            self.element_is_visible(self.locators.BIRTHDAY_YEAR_SEARCH).click()
+            self.element_is_visible(self.locators.BIRTHDAY_YEAR_SEARCH).click()
+            self.element_is_visible(self.locators.BIRTHDAY_YEAR_SELECT).click()
+            self.element_is_visible(self.locators.BIRTHDAY_MONTH_SELECT).click()
+            self.element_is_visible(self.locators.BIRTHDAY_DAY_SELECT).click()
+            self.element_is_visible(self.locators.PHONE1).send_keys(person.mobile)
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_INPUT).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_SEARCH).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_SEARCH).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_SEARCH_RIGHT).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_YEAR_SELECT).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_MONTH_SELECT).click()
+            self.element_is_visible(self.locators.LICENCE_EXPIRATION_DATE_DAY_SELECT).click()
+            self.element_is_visible(self.locators.LICENCE_NUMBER).send_keys(person.mobile)
+            self.element_is_visible(self.locators.LICENCE_TYPE_SELECT).click()
+            self.element_is_visible(self.locators.LICENCE_TYPE_OPTION).click()
+            self.element_is_visible(self.locators.HIRE_DATE_INPUT).click()
+            self.element_is_visible(self.locators.HIRE_DATE_INPUT_DAY_SELECT).click()
+            self.element_is_visible(self.locators.ROUTE_WITH_SEGMENTS).click()
+            self.element_is_visible(self.locators.CAN_SEE_AVAILABLE_TRIPS_SELECT).click()
+            self.element_is_visible(self.locators.CAN_SEE_AVAILABLE_TRIPS_OPTION).click()
+            self.element_is_visible(self.locators.EMAIL).send_keys(person.email)
+            self.element_is_visible(self.locators.USER_NAME).send_keys(username)
+            self.element_is_visible(self.locators.PASSWORD).send_keys(person.password)
+            self.element_is_visible(self.locators.CONFIRM_PASSWORD).send_keys(person.password)
+        with allure.step(f"click on save button"):
+            self.element_is_visible(self.locators.SAVE_BUTTON).click()
         return username
 
+    @allure.step("Checking Add Driver Form, taking the text message")
     def check_add_driver(self):
         result = self.element_is_present(self.locators.RESULT).text
         return result
--- tests/users_test.py
+++ tests/users_test.py
@@ -1,12 +1,16 @@
 from pages.users_page import AddUserPage
 from conftest import driver
 from pages.login_page import LoginPage
-from creds_and_links import Links
+from links import Links
+import allure
+import pytest
 
-
+@allure.feature("Add User")
 class TestUsers:
 
-    def test_add_new_driver(self, driver):
+    @pytest.mark.regression
+    @allure.title("Check Add User")
+    def test_add_driver(self, driver):
         login = LoginPage(driver, Links.login)
         login.open()
         login.fill_login_form()
@@ -16,6 +20,52 @@ class TestUsers:
         print(user)
         result = add_user.check_add_driver()
         assert result == f'Driver with #{user} was created.', 'The driver was not created'
+
+    @allure.title("Check Add Administrator")
+    def test_add_administrator(self, driver):
+        pass
+
+    @allure.title("Check Add Dispatcher")
+    def test_add_dispatcher(self, driver):
+        pass
+
+    @allure.title("Check Add Operator")
+    def test_add_operator(self, driver):
+        pass
+
+    @allure.title("Check Add Driver Group Owner")
+    def test_add_hr_specialist(self, driver):
+        pass
+
+    @allure.title("Check Add Mechanic")
+    def test_add_mechanic(self, driver):
+        pass
+
+    @allure.title("Check Add Biller")
+    def test_add_biller(self, driver):
+        pass
+
+    @allure.title("Check Add Driver Assistant")
+    def test_add_driver_group_owner(self, driver):
+        pass
+
+    @allure.title("Check Add Applicant")
+    def test_add_driver_assistant(self, driver):
+        pass
+
+    @allure.title("Check Add Attendant")
+    def test_add_applicant(self, driver):
+        pass
+
+    @allure.title("Check Add Attendant")
+    def test_add_attendant(self, driver):
+        pass
+
+    @allure.title("Check Add Case Manager")
+    def test_add_case_manager(self, driver):
+        pass
+
+
 
 
 
